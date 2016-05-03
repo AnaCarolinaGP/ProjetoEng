4d2461ae2b7f8f625ba598ec66e4834ad2491600
AnaCarolinaGP <ana.carolina_gp@hotmail.com>
2016-05-03 16:19:30 -0300

    Interface
    Interface do contador de caracteres conforme especificado

---
+++ Contador/util/funcoes.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+	
+	tamanhoTela = $(window).height();
+	larguraTela = $(window).width();
+	
+	/* AJUSTES NA TELA */
+	$("#principal").css("height", tamanhoTela - 110);	
+	$("#principal div").css("height", (tamanhoTela - 110) - 140 );	
+	$("#modal").css("left", (larguraTela - 520) / 2);	
+	
+});
