361058410556eba370e7ef9909fa6a486f7cc878
AnaCarolinaGP <ana.carolina_gp@hotmail.com>
2016-05-06 08:28:23 -0300

    Função de Parágrafos
    A cada tecla "enter" um parágrafo é inserido no texto

--- Contador/util/funcoes.js
+++ Contador/util/funcoes.js
@@ -8,4 +8,16 @@
 	$("#principal div").css("height", (tamanhoTela - 110) - 140 );	
 	$("#modal").css("left", (larguraTela - 520) / 2);	
 	
+	$("#paragrafo").keypress(function( event ) {
+		if (event.which == 13) {
+			event.preventDefault();
+			
+			campo = $(this).val();
+			
+			$("#principal div").append("<p>"+ campo +"</p>");
+			
+			$(this).val("");
+		}	
+	});
+	
 });
