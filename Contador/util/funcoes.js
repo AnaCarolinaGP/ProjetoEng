88698f46368211f2b4ab432a10a1f575dff8c251
AnaCarolinaGP <ana.carolina_gp@hotmail.com>
2016-05-16 14:35:10 -0300

    Função de contagem
    Função para exibição da contagem de caracteres (contagem simples, apenas
    o total de caracteres do texto)

--- Contador/util/funcoes.js
+++ Contador/util/funcoes.js
@@ -1,4 +1,7 @@
-﻿$(document).ready(function(){
+﻿// variaveis globais
+caracteresTotal = 0;
+
+$(document).ready(function(){
 	
 	tamanhoTela = $(window).height();
 	larguraTela = $(window).width();
@@ -12,12 +15,24 @@
 		if (event.which == 13) {
 			event.preventDefault();
 			
+			// armazena o paragrafo digitado
 			campo = $(this).val();
 			
+			// apresenta o paragrafo digitado dentro do texto
 			$("#principal div").append("<p>"+ campo +"</p>");
 			
+			// limpa o campo para edição de um novo paragrafo
 			$(this).val("");
+			
+			// total de caracteres digitados
+			qntCaracteresTotais = campo.length;
+			caracteresTotal += qntCaracteresTotais;
+	
 		}	
+	});
+	
+	$("#contar").click(function(){
+		alert("Total de caracteres: "+caracteresTotal);
 	});
 	
 });
